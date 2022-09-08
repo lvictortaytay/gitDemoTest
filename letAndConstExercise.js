9ed18627f9b74c40c556d4593ecd0b01ae3f52a0
lvictor taylor <lvictor.taylor@gmail.com>
2022-09-08 18:57:50 -0400

    Add files via upload

---
+++ letAndConstExercise.js
@@ -0,0 +1,28 @@
+//ES5 GLOBAL CONSTANTS 
+
+var PI = 3.14
+PI = 42   //stop me from doing this 
+//-----------------------------------
+
+//answer:
+const Pi = 3.14
+Pi = 42
+
+
+
+
+/*
+question 1: what is the difference between VAR and LET?
+    - var is a hoisted key word and can be called from anywhere in the scope, whereas let is block scoped meaning if it is within curly braces it cannot be 
+    accessed outside of it.
+
+question 2: what is the difference between VAR and CONST?
+     - const cannot be reassigned nor can it be redeclared, whereas var can be both reasigned as well as redeclared.
+
+question 3: what is the difference between LET and CONST?
+    - const cannot be reassigned whereas let can be.
+
+question 4: what is hoisting
+    - this is when the compiler lefts up the defined variable to the highest scope where it is defined in ( using the key word var )
+*/
+
